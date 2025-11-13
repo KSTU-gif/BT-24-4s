3c34aea799e4b97d763f9b884269df535bfedbcb
Roma Mursalov <romakotov55@gmail.com>
2025-11-13 12:09:55 +0500

    yaris

--- task1.py
+++ task1.py
@@ -1,14 +1,15 @@
+#2 nuska
 1)
 def fact (n);
  if == 0 or n == 1
-	return 1:
+    return 1:
 else:
-	return n*fact(n-1)
+    return n*fact(n-1)
 def sum(x, n):
-	if == 1:
-	return(1/1)+math.sqrt(abs(x))
+    if == 1:
+    return(1/1)+math.sqrt(abs(x))
 else:
-	return(1/fact(n))+math.sqrt(abs(x))+sum(x, n-1)
+    return(1/fact(n))+math.sqrt(abs(x))+sum(x, n-1)
 
 x=2
 n=4
