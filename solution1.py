b72390cf9cfe66f7690a687cd470b99a7cc703d3
Alfarabi Makashev <makashev_05@mail.ru>
2025-11-13 13:10:46 +0600

    dfsfsdf

--- solution1.py
+++ solution1.py
@@ -1,1 +1,13 @@
-print(1)
+#3 Вариант
+dif  k = input()
+     x = input()
+     fact = k
+
+	if fact == k:
+	return fact
+		
+     factorialk = k * fact
+		
+     res = 1 + (sin(k * x)/factorialk)
+
+print(res)
---
+++ solution2.py
@@ -0,0 +1,6 @@
+#3 Вариант
+dif nested_dict(n):
+    text = ("next")
+	if n > 0:
+	return nested_dict + text
+print(nested_dict)
