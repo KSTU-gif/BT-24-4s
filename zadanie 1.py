a241c775fa6482d79732a8c995860d8fd90c9e84
Ramzes404 <ramazanmax16@gmail.com>
2025-11-13 12:06:56 +0500

    task1

--- zadanie 1.py
+++ zadanie 1.py
@@ -1,1 +1,16 @@
-pringn gjugjg hj hfklyubldjfjg oidvjbp fh joegfn
+Zadanie 1 
+
+Zadanie 2 
+dif count_vowels(s) 
+vowels = "(abcабв)"
+if not s:
+return = 0
+return (1 if s(0) in vowels in else 0) + count_vowels(s[1:])  
+print (count_vowels("hello")) 
+
+
+
+
+
+
+
