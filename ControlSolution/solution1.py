5b8cfa93c5fd02a9ed4c78eee9f35c730898f360
SolomonJSX <janserik405@outlook.com>
2025-11-13 12:08:18 +0500

    SOlutions

---
+++ ControlSolution/solution1.py
@@ -0,0 +1,12 @@
+def solve(k = 1, n):
+	if k >= n:
+		return 0
+	else:
+		exp = (2 * k + 1) * (2 * k + 1)
+		return 1/exp + solve(k + 1; n)
+
+n = int(input("Введите n:"))
+
+result = solve(k, n)
+
+print(f"Результат: {result}")
