b2dd2d1b84652752b44e82cf993447bfc738122f
Roma Mursalov <romakotov55@gmail.com>
2025-11-13 12:07:48 +0500

    yaris

--- task1.py
+++ task1.py
@@ -1,1 +1,20 @@
-yaris yaris
+1)
+def fact (n);
+ if == 0 or n == 1
+	return 1:
+else:
+	return n*fact(n-1)
+def sum(x, n):
+	if == 1:
+	return(1/1)+math.sqrt(abs(x))
+else:
+	return(1/fact(n))+math.sqrt(abs(x))+sum(x, n-1)
+
+x=2
+n=4
+print(sum(x,n))
+2)
+def sum_digits(n):
+    if num < 10:
+	return 10
+
