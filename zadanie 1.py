befb1b8432321d4f6d58eee7f37ffa6bcb9d91a4
Ramzes404 <ramazanmax16@gmail.com>
2025-12-04 10:59:59 +0500

    t1esting commit

--- zadanie 1.py
+++ zadanie 1.py
@@ -1,7 +1,7 @@
 #Variant 1 
 #Zadanie 2 
-dif count_vowels(s) 
-vowels = "(abcабв)"
+def count_vowels(s): 
+vowels = 'abcабв'
 if not s:
 return = 0
 return (1 if s(0) in vowels in else 0) + count_vowels(s[1:])  
